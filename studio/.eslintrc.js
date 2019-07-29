6250a0cb36e1e245ffd470dc67899d4dbe07ea9c
olemagnus <olestovern@gmail.com>
2019-07-29 12:26:48 +0200

    Makes prettier/eslint use cra setup

--- studio/.eslintrc.js
+++ studio/.eslintrc.js
@@ -1,16 +1,12 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  extends: ['standard', 'standard-react'],
+  extends: ['react-app'],
   parser: 'babel-eslint',
-  rules: {
-    'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
-  },
   settings: {
     react: {
       pragma: 'React',
       version: '16.2.0'
     }
   }
-}
+};
--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,13 +1,9 @@
 module.exports = {
-  extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
-  rules: {
-    'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
-  },
+  extends: ['react-app'],
   settings: {
     react: {
       pragma: 'React',
       version: '16.8.4'
     }
   }
-}
+};
