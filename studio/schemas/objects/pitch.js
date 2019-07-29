50aa394594448989bcd043af5084bdbe19cb67b1
olemagnus <olestovern@gmail.com>
2019-07-29 17:24:05 +0200

    adds large style

--- studio/schemas/objects/pitch.js
+++ studio/schemas/objects/pitch.js
@@ -18,6 +18,10 @@ export default {
             {
               title: 'Normal',
               value: 'normal'
+            },
+            {
+              title: 'Large',
+              value: 'large'
             }
           ],
           lists: [],
