666dd6eff84a6dd5f22e045487e0463b59a9f74a
Netlife Design <netlifedesign@Netlifes-MacBook-Pro-5.local>
2019-08-05 13:18:21 +0200

    adds portable text and responsive image

---
+++ web/src/templates/office.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Container from '../components/container';
+import GraphQLErrorList from '../components/graphql-error-list';
+import SEO from '../components/seo';
+import Layout from '../containers/layout';
+import { toPlainText } from '../lib/helpers';
+import PortableText from '../components/portableText';
+import Img from 'gatsby-image';
+
+export const query = graphql`
+  {
+    sanityContact {
+      heading
+      offices {
+        name
+        _rawOfficeInfo
+        image {
+          asset {
+            fluid(maxWidth: 1706) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+      _rawPitch
+    }
+  }
+`;
+
+const OfficeTemplate = props => {
+  const { data, errors } = props;
+
+  const { heading, offices } = data.sanityContact;
+
+  return (
+    <>
+      <SEO title={'Kontakt'} description={heading} />
+      <Layout>
+        {errors && <SEO title="GraphQL Error" />}
+        <h1 className="text-xl">{heading}</h1>
+
+        {errors && (
+          <Container>
+            <GraphQLErrorList errors={errors} />
+          </Container>
+        )}
+
+        {offices.map(office => (
+          <section className="flex flex-wrap mt-16 -mx-4">
+            <div className="w-full order-2 md:order-1 md:w-1/3 px-4">
+              <h2 className="text-lg mt-2 md:mt-0">{office.name}</h2>
+              <PortableText blocks={office._rawOfficeInfo} />
+            </div>
+            <div className="w-full order-1 md:order-2 md:w-2/3 px-4">
+              <Img fluid={office.image.asset.fluid} />
+            </div>
+          </section>
+        ))}
+      </Layout>
+    </>
+  );
+};
+
+export default OfficeTemplate;
