fdb5817cee261a4fbf406338115a4756eef528ab
olemagnus <olestovern@gmail.com>
2019-08-09 10:26:51 +0200

    adjusts lineheight

--- web/src/templates/contact.js
+++ web/src/templates/contact.js
@@ -4,7 +4,6 @@ import Container from '../components/container';
 import GraphQLErrorList from '../components/graphql-error-list';
 import SEO from '../components/seo';
 import Layout from '../containers/layout';
-import { toPlainText } from '../lib/helpers';
 import PortableText from '../components/portableText';
 import Img from 'gatsby-image';
 
@@ -17,7 +16,7 @@ export const query = graphql`
         _rawOfficeInfo
         image {
           asset {
-            fluid(maxWidth: 1706) {
+            fluid(maxWidth: 1440) {
               ...GatsbySanityImageFluid
             }
           }
@@ -49,9 +48,7 @@ const ContactTemplate = props => {
         {offices.map(office => (
           <section className="flex flex-wrap mt-12 -mx-4">
             <div className="w-full order-2 md:order-1 md:w-1/3 px-4">
-              <h2 className="text-lg mt-6 md:mt-0 mb-4 leading-extra-none">
-                {office.name}
-              </h2>
+              <h2 className="text-lg mb-4 -mt-2">{office.name}</h2>
               <PortableText blocks={office._rawOfficeInfo} />
             </div>
             <div className="w-full order-1 md:order-2 md:w-2/3 px-4">
