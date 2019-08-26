23d6ec0f713c9f6fca075c346a4ef10ba5f4ca6a
olemagnus <olestovern@gmail.com>
2019-08-26 18:29:16 +0200

    switches to textImage field

--- studio/schemas/documents/contact.js
+++ studio/schemas/documents/contact.js
@@ -2,6 +2,7 @@ export default {
   name: 'contact',
   type: 'document',
   title: 'Contact',
+  __experimental_actions: ['update', 'publish'],
   fields: [
     {
       name: 'heading',
@@ -22,9 +23,11 @@ export default {
       ]
     },
     {
-      name: 'pitch',
-      title: 'Pitch',
-      type: 'pitch'
+      name: 'textImage',
+      type: 'array',
+      of: [{ type: 'textImage' }],
+      description: 'Used for additional info, or linking to relevant content.',
+      validation: Rule => Rule.required().max(1)
     }
   ]
 };
