93c64683706e0496731f9ad820d8963030be56b5
olemagnus <olestovern@gmail.com>
2019-08-28 14:15:15 +0200

    removes unused import

--- web/src/components/Image.js
+++ web/src/components/Image.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import { imageUrlFor } from '../lib/image-url';
 import { buildImageObj } from '../lib/helpers';
 
