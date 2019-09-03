04a5c950a4651a5e8b400f624adfe82f3312c01d
olemagnus <olestovern@gmail.com>
2019-09-03 17:45:41 +0200

    adds rich text object

---
+++ studio/schemas/objects/richText.js
@@ -0,0 +1,42 @@
+import { toPlainText } from '../../../web/src/lib/helpers';
+
+export default {
+  name: 'richText',
+  title: 'Rich Text',
+  type: 'object',
+  fields: [
+    {
+      name: 'textContent',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'H1', value: 'h1' },
+            { title: 'H2', value: 'h2' },
+            { title: 'H3', value: 'h3' }
+          ],
+          lists: [
+            { title: 'Numbered', value: 'number' },
+            { title: 'Bullet', value: 'bullet' }
+          ],
+          marks: {
+            decorators: [{ title: 'Strong', value: 'strong' }]
+          }
+        }
+      ]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'textContent'
+    },
+    prepare(selection) {
+      const { title } = selection;
+      return {
+        title: toPlainText(title)
+      };
+    }
+  }
+};
