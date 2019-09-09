9f2fb717a803810ca5a03598c7f186ef49ec28d7
olemagnus <olestovern@gmail.com>
2019-09-09 10:13:31 +0200

    adds nofollow and internal link option

--- studio/schemas/objects/link.js
+++ studio/schemas/objects/link.js
@@ -3,6 +3,34 @@ export default {
   type: 'object',
   fields: [
     { type: 'string', name: 'title', title: 'Title' },
-    { type: 'string', name: 'url', title: 'URL' }
+    { type: 'string', name: 'text', title: 'Text' },
+    { type: 'string', name: 'url', title: 'URL' },
+    {
+      type: 'boolean',
+      name: 'internalLink',
+      title: 'Internal Link',
+      description: 'Uses the reference field instead of the URL field.'
+    },
+    {
+      type: 'boolean',
+      name: 'nofollow',
+      title: 'No Follow',
+      description: 'For SEO. Makes search engines ignore this path.'
+    },
+    {
+      type: 'reference',
+      title: 'Internal page',
+      name: 'internalPage',
+      to: [
+        { type: 'article' },
+        { type: 'office' },
+        { type: 'contact' },
+        { type: 'person' },
+        { type: 'jobAdvert' },
+        { type: 'jobAdvertListing' },
+        { type: 'newsletter' },
+        { type: 'blogPost' }
+      ]
+    }
   ]
 };
