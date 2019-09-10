b397416793a5de0ce67ad6fdc00f1c80aaa61f30
olemagnus <olestovern@gmail.com>
2019-09-10 15:58:03 +0200

    fixes name

--- web/src/templates/article.js
+++ web/src/templates/article.js
@@ -3,7 +3,7 @@ import SEO from '../components/seo';
 import Layout from '../containers/layout';
 import PortableText from '../components/PortableText';
 
-const personBio = ({ pageContext, location }) => {
+export default ({ pageContext, location }) => {
   const { name: name = '', title: title = '', _rawText } = pageContext;
   return (
     <>
@@ -11,11 +11,9 @@ const personBio = ({ pageContext, location }) => {
       <Layout>
         <h1 className="text-xl -mt-2 mb-8 w-full md:w-2/3">{title}</h1>
         <section className="mx-auto w-full sm:w-3/4 lg:w-1/2">
-          <PortableText blocks={_rawText} />
+          <PortableText blocks={_rawText.richText} />
         </section>
       </Layout>
     </>
   );
 };
-
-export default personBio;
