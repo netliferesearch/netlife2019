7a1670a4f9fbf4abe5b1997e891fc952c184d690
olemagnus <olestovern@gmail.com>
2019-09-16 14:43:36 +0200

    adds hand animation

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import SpriteAnimator from 'react-sprite-animator';
 import SEO from '../components/seo';
 import Layout from '../containers/layout';
 import Link from '../components/Link';
+import Animation from '../components/Animation';
 
 export default ({ location }) => {
   const { site, mainMenu } = useStaticQuery(
@@ -20,7 +20,6 @@ export default ({ location }) => {
     `
   );
 
-  const siteTitle = site?.title || '';
   const seo = site?._rawHomePageSeo || null;
   const menuItems = mainMenu?.items || [];
 
@@ -28,8 +27,8 @@ export default ({ location }) => {
     <>
       <SEO seo={seo} location={location} />
       <Layout>
-        <div className="flex flex-wrap">
-          <nav className="w-full md:w-1/2 pt-0 md:pt-8">
+        <div className="flex flex-wrap pt-0 md:pt-8">
+          <nav className="w-full md:w-1/2">
             {menuItems.map(item => (
               <div key={item._key}>
                 <Link
@@ -43,13 +42,12 @@ export default ({ location }) => {
             ))}
           </nav>
           <div className="flex w-full md:w-1/2 text-left md:text-right flex-col justify-center">
-            <div className="md:self-center mt-8 md:mt-0">
-              <SpriteAnimator
-                shouldAnimate={true}
-                fps={10}
-                sprite="./home-hand-scroll.svg"
+            <div className="mr-0 md:ml-16 lg:ml-48 mt-16 md:mt-2 lg:mt-0">
+              <Animation
+                sprite="/hand-scroll.svg"
+                baseScale={1}
+                height={194}
                 width={280}
-                height={280}
               />
             </div>
           </div>
