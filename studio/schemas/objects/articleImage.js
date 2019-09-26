95775bc6387d53be903378946b6eca7e1992717c
olemagnus <olestovern@gmail.com>
2019-09-26 13:11:22 +0200

    makes use of radio layout

--- studio/schemas/objects/articleImage.js
+++ studio/schemas/objects/articleImage.js
@@ -34,7 +34,8 @@ export default {
       title: 'Aspect Ratio',
       description: 'Default is 3:2',
       options: {
-        list: [{ title: '3:2', value: '3:2' }, { title: '1:1', value: '1:1' }]
+        list: ['3:2', '1:1'],
+        layout: 'radio'
       }
     }
   ]
