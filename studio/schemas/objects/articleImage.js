2c6fa052d87e2216fd99bdf955c9c8757fe21fa8
olemagnus <olestovern@gmail.com>
2019-09-26 11:09:48 +0200

    adds aspect ratio field

--- studio/schemas/objects/articleImage.js
+++ studio/schemas/objects/articleImage.js
@@ -27,6 +27,15 @@ export default {
       type: 'boolean',
       title: 'Large Image',
       description: 'Should the image span outside its parent text container'
+    },
+    {
+      name: 'aspectRatio',
+      type: 'string',
+      title: 'Aspect Ratio',
+      description: 'Default is 3:2',
+      options: {
+        list: [{ title: '3:2', value: '3:2' }, { title: '1:1', value: '1:1' }]
+      }
     }
   ]
 };
