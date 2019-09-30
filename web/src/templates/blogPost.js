57196ecc77ba319353d06e8c80d79507b7a37c7d
olemagnus <olestovern@gmail.com>
2019-09-30 17:50:42 +0200

    adds categories and persons

--- web/src/templates/blogPost.js
+++ web/src/templates/blogPost.js
@@ -4,6 +4,8 @@ import SEO from '../components/seo';
 import Layout from '../containers/layout';
 import PortableText from '../components/PortableText';
 import Image from '../components/Image';
+import Person from '../components/Person';
+import Link from '../components/Link';
 import { formatFullDate } from '../lib/formatDates/formatDates';
 
 // Non static query, see $id
@@ -20,6 +22,12 @@ export const query = graphql`
           ...ImageFragment
         }
         alt
+      }
+      serviceCategories {
+        title
+        slug {
+          current
+        }
       }
       _rawArticle(resolveReferences: { maxDepth: 10 })
       _rawSeo(resolveReferences: { maxDepth: 5 })
@@ -46,7 +54,9 @@ export default ({ data, pageContext, location }) => {
     mainImage: { image: mainImage = null, alt: mainImageAlt = '' },
     _rawArticle: { textContent: textContent = null } = {},
     _rawSeo: seo = null,
-    publishDate: publishDate = ''
+    serviceCategories: serviceCategories = [],
+    publishDate: publishDate = '',
+    author: persons = []
   } = data?.sanityBlogPost;
 
   return (
@@ -54,14 +64,46 @@ export default ({ data, pageContext, location }) => {
       <SEO seo={seo} location={location} />
       <Layout breadcrumb={pageContext.breadcrumb}>
         <div className="w-full max-w-full lg:max-w-lg mx-auto">
-          <h1 className="text-xl -mt-2 mb-4">{title}</h1>
-          <div className="mb-6">
-            Publisert:{' '}
-            <date timedate={publishDate}>{formatFullDate(publishDate)}</date>
-          </div>
-          {mainImage && <Image image={mainImage} alt={mainImageAlt} />}
-          <section className="mt-12">
-            <PortableText blocks={textContent} />
+          <article>
+            <h1 className="text-xl -mt-2 mb-4">{title}</h1>
+            <div className="mb-6">
+              Publisert:{' '}
+              <date timedate={publishDate}>{formatFullDate(publishDate)}</date>
+            </div>
+            {mainImage && <Image image={mainImage} alt={mainImageAlt} />}
+            <section className="mt-12">
+              <PortableText blocks={textContent} />
+            </section>
+          </article>
+          <section className="border-t mt-8">
+            {persons.map((person, index) => (
+              <div key={person._id} className="px-4 mt-8">
+                <Person
+                  name={person.name}
+                  email={person.email}
+                  role={person.role}
+                  slug={person.slug.current}
+                  services={person.services}
+                  phoneNumber={person.phoneNumber}
+                  image={person.image}
+                />
+              </div>
+            ))}
+          </section>
+          <section className="border-t mt-8 pt-8">
+            <h2 className="text-lg mb-2">Tjenester vi tilbyr:</h2>
+            <ul>
+              {serviceCategories.map(service => (
+                <li className="list-disc ml-4">
+                  <Link
+                    slug={service.slug.current}
+                    className="font-lining link"
+                  >
+                    {service.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </section>
         </div>
       </Layout>
