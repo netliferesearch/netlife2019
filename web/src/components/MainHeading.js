27668cce5786d354ca13a2d5c301dce52366cc14
olemagnus <olestovern@gmail.com>
2019-09-30 12:51:23 +0200

    removes unnecessary class

--- web/src/components/MainHeading.js
+++ web/src/components/MainHeading.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 const headingClasses = tight =>
   classNames({
-    'text-xl -mt-2 w-full md:w-3/4': true,
+    'text-xl -mt-2 md:w-3/4': true,
     'mb-12 md:mb-16': !tight,
     'mb-8': tight
   });
