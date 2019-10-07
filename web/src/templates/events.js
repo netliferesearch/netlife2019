da60f941e32207356bbe880d4a694faf194d5e03
olemagnus <olestovern@gmail.com>
2019-10-07 14:08:55 +0200

    removes id query

--- web/src/templates/events.js
+++ web/src/templates/events.js
@@ -12,7 +12,7 @@ const Events = ({ pageContext, location }) => {
   const { sanityEventListing } = useStaticQuery(
     graphql`
       {
-        sanityEventListing(_id: { eq: "eventListing" }) {
+        sanityEventListing {
           id
           events {
             _id
