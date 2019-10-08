281feef01048f0882455b8343a7d180a7b69d6bc
olemagnus <olestovern@gmail.com>
2019-10-08 13:38:59 +0200

    Adds form page

---
+++ web/src/templates/formPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import SEO from '../components/seo';
+import Layout from '../containers/layout';
+import PortableText from '../components/PortableText';
+import MainHeading from '../components/MainHeading';
+
+// Non static query, see $id
+export const query = graphql`
+  query($id: String!) {
+    sanityFormPage(id: { eq: $id }) {
+      title
+      _rawText(resolveReferences: { maxDepth: 10 })
+      _rawSeo(resolveReferences: { maxDepth: 5 })
+    }
+  }
+`;
+
+export default ({ data, location }) => {
+  const {
+    title: title = '',
+    _rawText: { textContent: textContent = null } = {},
+    _rawSeo: seo = null
+  } = data?.sanityFormPage;
+
+  return (
+    <>
+      <SEO title={title} seo={seo} location={location} />
+      <Layout>
+        <MainHeading>{title}</MainHeading>
+        <section className="mx-auto w-full sm:w-3/4 lg:w-1/2">
+          <PortableText blocks={textContent} />
+        </section>
+      </Layout>
+    </>
+  );
+};
