43979a7169cc2e9dd70fb97d1f64ed36649104a0
Tatsumi Suzuki <tatsumi.suzuki@netlife.com>
2019-11-11 10:34:30 +0100

    Add gatsby netlify plugin for customisations.

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-advanced-sitemap',
     'gatsby-plugin-google-tagmanager',
+    'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-source-sanity',
       options: {
@@ -61,6 +62,18 @@ module.exports = {
         // should be an object or a function that is executed in the browser
         // Defaults to null
         defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify",
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
     },
   ]
