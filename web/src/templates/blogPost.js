e89ea36c3af3801054958377f2189c186615853c
Wallace Sidhrée <i@dreamyguy.com>
2020-04-02 11:02:20 +0200

    :shower: Separate sections within 'blogPost' and so that we can eventually create reusable components

--- web/src/templates/blogPost.js
+++ web/src/templates/blogPost.js
@@ -13,15 +13,17 @@ export const query = graphql`
   query($id: String!) {
     sanityBlogPost(id: { eq: $id }) {
       title
-      publishDate
       slug {
         current
       }
+      publishDate
+      mainImageText
       mainImage {
         image {
           ...ImageFragment
         }
         alt
+        aspectRatio
       }
       serviceCategories {
         name
@@ -43,19 +45,85 @@ export const query = graphql`
         }
         role
       }
+      intro
     }
   }
 `;
 
-export default ({ data, pageContext, location }) => {
+const rendetTop = ({ mainImage, mainImageAlt, publishDate, title }) => {
+  return (
+    <>
+      <h1 className="text-xl -mt-2 mb-4">{title}</h1>
+      <div className="mb-6">
+        Publisert:{' '}
+        <span timedate={publishDate}>{formatFullDate(publishDate)}</span>
+      </div>
+      {mainImage?.asset && <Image image={mainImage} alt={mainImageAlt} />}
+    </>
+  );
+};
+
+const rendetContent = textContent => (
+  <section className="mt-12">
+    <PortableText blocks={textContent} />
+  </section>
+);
+
+const renderPersons = persons => {
+  return (
+    <section className="border-t mt-8">
+      {persons.map(person => (
+        <div key={person.id} className="px-4 mt-8">
+          <Person
+            name={person.name}
+            email={person.email}
+            role={person.role}
+            slug={person.slug.current}
+            services={person.services}
+            phoneNumber={person.phoneNumber}
+            image={person.image}
+            inactiveUser={person.inactive}
+          />
+        </div>
+      ))}
+    </section>
+  );
+};
+
+const renderServices = serviceCategories => {
+  return (
+    <section className="border-t mt-8 pt-8">
+      <h2 className="text-lg mb-2">Tjenester vi tilbyr:</h2>
+      <ul>
+        {serviceCategories.map(service => (
+          <li className="list-disc ml-4" key={service}>
+            <Link
+              slug={'/'} // TODO: Make it use a real slug
+              className="font-lining link"
+            >
+              {service.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+const blogPost = ({ data, pageContext, location }) => {
   const {
     title: title = '',
-    mainImage: { image: mainImage = null, alt: mainImageAlt = '' },
+    mainImage: {
+      image: mainImage = null,
+      alt: mainImageAlt = '',
+      aspectRatio = null
+    },
     _rawArticle: { textContent: textContent = null } = {},
     _rawSeo: seo = null,
     serviceCategories: serviceCategories = [],
     publishDate: publishDate = '',
-    author: persons = []
+    author: persons = [],
+    intro: intro = null
   } = data?.sanityBlogPost;
 
   return (
@@ -64,49 +132,20 @@ export default ({ data, pageContext, location }) => {
       <Layout breadcrumb={pageContext.breadcrumb}>
         <div className="w-full max-w-full">
           <article>
-            <h1 className="text-xl -mt-2 mb-4">{title}</h1>
-            <div className="mb-6">
-              Publisert:{' '}
-              <span timedate={publishDate}>{formatFullDate(publishDate)}</span>
-            </div>
-            {mainImage?.asset && <Image image={mainImage} alt={mainImageAlt} />}
-            <section className="mt-12">
-              <PortableText blocks={textContent} />
-            </section>
+            {rendetTop({
+              mainImage,
+              mainImageAlt,
+              publishDate,
+              title
+            })}
+            {rendetContent(textContent)}
           </article>
-          <section className="border-t mt-8">
-            {persons.map(person => (
-              <div key={person.id} className="px-4 mt-8">
-                <Person
-                  name={person.name}
-                  email={person.email}
-                  role={person.role}
-                  slug={person.slug.current}
-                  services={person.services}
-                  phoneNumber={person.phoneNumber}
-                  image={person.image}
-                  inactiveUser={person.inactive}
-                />
-              </div>
-            ))}
-          </section>
-          <section className="border-t mt-8 pt-8">
-            <h2 className="text-lg mb-2">Tjenester vi tilbyr:</h2>
-            <ul>
-              {serviceCategories.map(service => (
-                <li className="list-disc ml-4" key={service}>
-                  <Link
-                    slug={'/'} // TODO: Make it use a real slug
-                    className="font-lining link"
-                  >
-                    {service.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </section>
+          {renderPersons(persons)}
+          {renderServices(serviceCategories)}
         </div>
       </Layout>
     </>
   );
 };
+
+export default blogPost;
