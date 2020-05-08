8820d61fbcb5c414f1acca3ca26b45a8536c3012
Lars Tollefsen <larsthomastt@gmail.com>
2020-05-08 15:27:28 +0200

    Updated services page to the requested specifications

--- web/src/templates/services.js
+++ web/src/templates/services.js
@@ -7,45 +7,22 @@ import TextImageIntroContainer from '../containers/TextImageIntroContainer';
 import FeaturedContainer from '../containers/FeaturedContainer';
 import Link from '../components/Link';
 import ContactSection from '../components/ContactSection';
+import PortableText from '../components/PortableText'
+import Image from '../components/Image'
 import path from 'path';
 import { showTemplateName } from '../lib/showTemplateNameUtil';
 const templateName = path.basename(__filename);
 
 export default ({ pageContext, location }) => {
   showTemplateName(templateName);
-  const { page, services, contact } = useStaticQuery(
+  const { page, contact } = useStaticQuery(
     graphql`
       query {
-        page: sanityOurServices {
-          title
+        page:   sanityOurServices {
           heading
-          _rawSeo(resolveReferences: { maxDepth: 5 })
-          _rawAdditionalContent(resolveReferences: { maxDepth: 5 })
-          featuredCases {
-            id
-            title
-            slug {
-              current
-            }
-            _rawIntro(resolveReferences: { maxDepth: 5 })
-            mainImage {
-              image {
-                ...ImageFragment
-              }
-              alt
-              aspectRatio
-            }
+          intro
+          _rawContent
           }
-        }
-        services: allSanityService {
-          nodes {
-            id
-            title
-            slug {
-              current
-            }
-          }
-        }
         contact: sanitySiteSettings {
           contactBlock {
             _key
@@ -97,12 +74,12 @@ export default ({ pageContext, location }) => {
     `
   );
 
+  const {
+    heading: heading = '',
+    _rawContent: content = [],
+  } = page
   const seo = page?._rawSeo || null;
-  const additionalContent = page?._rawAdditionalContent || null;
-  const featuredCases = page?.featuredCases || null;
-  const title = page?.title || '';
-  const heading = page?.heading || '';
-  const ourServices = services?.nodes || [];
+  const intro = page?.intro || '';
   const {
     form: form = null,
     persons: defaultContactPersons = [],
@@ -111,40 +88,65 @@ export default ({ pageContext, location }) => {
 
   return (
     <>
-      <SEO title={title} description="" seo={seo} location={location} />
+      <SEO title={heading} description="" seo={seo} location={location} />
       <Layout breadcrumb={pageContext.breadcrumb}>
-        <MainHeading tight>{heading}</MainHeading>
-        {ourServices && (
-          <ul>
-            {ourServices.map((service) => (
-              <li key={service.id} className="inline-block border -ml-px-2 -mt-px-2">
-                {service.slug?.current ? (
-                  <Link slug={service.slug.current} title={service.title} className="block px-2 py-2 hover:bg-green focus:bg-green">
-                    {service.title}
-                  </Link>
-                ) : (
-                  <div className="px-2 py-2">
-                    {service.title}
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-        {additionalContent && (
-          <TextImageIntroContainer
-            image={additionalContent?.image}
-            alt={additionalContent?.alt}
-            imageLeft={additionalContent?.imageLeft}
-            isHalf
-            textContent={additionalContent?.textContent}
-          />
-        )}
-        {featuredCases && (
-          <section className="mb-16">
-            <FeaturedContainer posts={featuredCases} />
+
+
+        <article>
+
+          <section className="w-3/4">
+            <MainHeading tight>{heading}</MainHeading>
+            <section className="w-2/5">{intro}</section>
           </section>
-        )}
+
+          <section className="w-full mt-16 border-solid border-black border-t">
+            {content &&
+              content.map(c => {
+                if (c._type === 'richText') {
+                  return (
+                    <div
+                      className="my-8 md:my-16 md:w-2/3 xl:w-1/2 mx-auto "
+                      key={c._key}
+                    >
+                      <div>
+                        {c?.heading && (
+                          <h2 className="text-md mb-4">{c.heading}</h2>
+                        )}
+                        <PortableText blocks={c.textContent} />
+                      </div>
+                    </div>
+                  )
+                } else if (c._type === 'imageObject' && c.image?.asset) {
+                  return (
+                    <div className="my-8 md:my-16 mx-auto" key={c._key}>
+                      <Image
+                        image={c.image}
+                        alt={c.image?.alt}
+                        aspectRatio={c?.aspectRatio}
+                      />
+                    </div>
+                  );
+                } else if (c._type === "richTextX4") {
+                  return (
+                    <div className="flex flex-wrap" >
+                      {c.textContent.map(x4Block => {
+                        return (
+                          <div key={x4Block._key} className="pl-16 pr-16 my-8 w-full md:w-full lg:w-1/2 ">
+                            {x4Block?.heading && (
+                              <h2 className="text-md mb-4">{x4Block.heading}</h2>
+                            )}
+                            <PortableText blocks={x4Block.textContent} />
+                          </div>
+                        )
+                      })}
+                    </div>
+                  )
+                }
+              })}
+          </section>
+
+        </article>
+
         {form && (
           <div className="mt-16 py-16 border-solid border-black border-t">
             <ContactSection
