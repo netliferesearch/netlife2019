a527dd9658c89b3bc453a8c9774f5724fb0177ea
Wallace Sidhrée <i@dreamyguy.com>
2020-05-08 10:31:16 +0200

    :sparkles: Introduce additional content to 'event' template, adding the possibility to define 'customEvent'

--- web/src/templates/event.js
+++ web/src/templates/event.js
@@ -39,6 +39,13 @@ export const query = graphql`
           current
         }
       }
+      additionalContent {
+        ... on SanityCustomEvent {
+          _key
+          _type
+        }
+      }
+      _rawAdditionalContent
     }
   }
 `;
@@ -58,7 +65,9 @@ const event = ({ data, pageContext, location }) => {
     _rawText: _rawText = null,
     outroImage: outroImage = null,
     _rawSeo: seo = null,
-    linkedPersons: linkedPersons = null
+    linkedPersons: linkedPersons = null,
+    _rawAdditionalContent: _rawAdditionalContent = [],
+    additionalContent: additionalContent = []
   } = data?.sanityEvent;
 
   return (
@@ -91,9 +100,7 @@ const event = ({ data, pageContext, location }) => {
         </div>
         <section className="mx-auto w-full sm:w-3/4 lg:w-1/2">
           <PortableText blocks={_rawText} />
-
           <hr className="mt-12 mb-6 border-t border-solid border-black" />
-
           {linkedPersons && linkedPersons.length > 0 && (
             <div className="my-8">
               <h3 className="text-md mb-4">Kursholder</h3>
@@ -115,7 +122,6 @@ const event = ({ data, pageContext, location }) => {
             </div>
           )}
           <hr className="mt-12 mb-6 border-t border-solid border-black" />
-
           <div className="flex flex-col justify-between mt-12 lg:w-2/3">
           <h3 className="text-md mb-4">Meld deg på</h3>
               <EventInfo
@@ -141,6 +147,44 @@ const event = ({ data, pageContext, location }) => {
             <Image image={outroImage} alt="test" aspectRatio="2:1" />
           </div>
         )}
+        {additionalContent.map(content => {
+          /* We need to use the raw field to render this objects block field */
+          if (content._type === 'customEvent') {
+            const rawContent = _rawAdditionalContent.find(
+              x => x._key === content._key
+            );
+
+            if (!rawContent) return null;
+
+            const {
+              alt,
+              aspectRatio,
+              image,
+              isHalf = true,
+              imageLeft,
+              imageText,
+              title,
+              text,
+            } = rawContent;
+
+            return (
+              <div className="py-8 md:py-16 border-t border-b border-solid border-black" key={content._key}>
+                <TextImage
+                  alt={alt}
+                  aspectRatio={aspectRatio}
+                  image={image}
+                  isHalf={isHalf}
+                  imageLeft={imageLeft}
+                  imageText={imageText}
+                >
+                  <h2 className="text-md mb-4 mt-4">{title}</h2>
+                  <PortableText blocks={text} />
+                </TextImage>
+              </div>
+            );
+          }
+          return null;
+        })}
       </Layout>
     </>
   );
