66ae4d090499dccef3dd921357d69791283e9a0e
Lars Tollefsen <larsthomastt@gmail.com>
2020-05-12 09:10:44 +0200

    Updated contact form and added additional content to services

--- web/src/templates/services.js
+++ web/src/templates/services.js
@@ -6,6 +6,7 @@ import MainHeading from '../components/MainHeading';
 import TextImageIntroContainer from '../containers/TextImageIntroContainer';
 import FeaturedContainer from '../containers/FeaturedContainer';
 import Link from '../components/Link';
+import TextImage from '../components/TextImage';
 import ContactSection from '../components/ContactSection';
 import PortableText from '../components/PortableText'
 import Image from '../components/Image'
@@ -22,7 +23,22 @@ export default ({ pageContext, location }) => {
           heading
           intro
           _rawContent
+          _rawAdditionalContent
+          additionalContent {
+            ... on SanityContactSection {
+              _key
+              _type
+            }
+            ... on SanityCustomEvent {
+              _key
+              _type
+            }
+            ... on SanityTextImage {
+              _key
+              _type
+            }
           }
+        }
         contact: sanitySiteSettings {
           contactBlock {
             _key
@@ -77,14 +93,12 @@ export default ({ pageContext, location }) => {
   const {
     heading: heading = '',
     _rawContent: content = [],
+    _rawAdditionalContent: _rawAdditionalContent = [],
+    additionalContent: additionalContent = []
   } = page
   const seo = page?._rawSeo || null;
   const intro = page?.intro || '';
-  const {
-    form: form = null,
-    persons: defaultContactPersons = [],
-    title: defaultContactTitle = null,
-  } = contact?.contactBlock;
+  const { form: form = null } = contact?.contactBlock;
 
   return (
     <>
@@ -94,9 +108,9 @@ export default ({ pageContext, location }) => {
 
         <article>
 
-          <section className="w-3/4">
+          <section className="w-full md:w-3/4">
             <MainHeading tight>{heading}</MainHeading>
-            <section className="w-2/5">{intro}</section>
+            <section className="w-full md:w-2/5">{intro}</section>
           </section>
 
           <section className="w-full mt-16 border-solid border-black border-t">
@@ -129,33 +143,106 @@ export default ({ pageContext, location }) => {
                 } else if (c._type === "richTextX4") {
                   return (
                     <div className="flex flex-wrap" >
-                      {c.textContent.map(x4Block => {
-                        return (
-                          <div key={x4Block._key} className="pl-16 pr-16 my-8 w-full md:w-full lg:w-1/2 ">
-                            {x4Block?.heading && (
-                              <h2 className="text-md mb-4">{x4Block.heading}</h2>
-                            )}
-                            <PortableText blocks={x4Block.textContent} />
-                          </div>
-                        )
-                      })}
+                      {c.textContent.map(x4Block => (
+                        <div key={x4Block._key} className="w-full my-4 md:pl-16 md:pr-16 md:my-8 lg:w-1/2 ">
+                          {x4Block?.heading && (
+                            <h2 className="text-md mb-4">{x4Block.heading}</h2>
+                          )}
+                          <PortableText blocks={x4Block.textContent} />
+                        </div>
+                      )
+                      )}
                     </div>
                   )
                 }
+                return null;
               })}
           </section>
 
         </article>
 
-        {form && (
-          <div className="mt-16 py-16 border-solid border-black border-t">
-            <ContactSection
-              form={form}
-              heading={defaultContactTitle}
-              persons={defaultContactPersons}
-            />
-          </div>
-        )}
+        {additionalContent.map(content => {
+          /* We need to use the raw field to render this objects block field */
+          if (content._type === 'textImage') {
+            const rawContent = _rawAdditionalContent.find(
+              x => x._key === content._key
+            );
+
+            if (!rawContent) return null;
+
+            return (
+              <div className="py-8 md:py-16" key={content._key}>
+                <TextImage
+                  image={rawContent.image}
+                  alt={rawContent.alt}
+                  imageLeft={rawContent.imageLeft}
+                >
+                  <h2 className="text-md mb-4 -mt-2">{rawContent.name}</h2>
+                  <PortableText blocks={rawContent.textContent} />
+                </TextImage>
+              </div>
+            );
+          } else if (content._type === 'customEvent') {
+            const rawContent = _rawAdditionalContent.find(
+              x => x._key === content._key
+            );
+
+            if (!rawContent) return null;
+
+            const {
+              alt,
+              aspectRatio,
+              image,
+              isHalf = true,
+              imageLeft,
+              imageText,
+              title,
+              text,
+            } = rawContent;
+
+            return (
+              <div className="py-8 md:py-16 border-t border-b border-solid border-black" key={content._key}>
+                <TextImage
+                  alt={alt}
+                  aspectRatio={aspectRatio}
+                  image={image}
+                  isHalf={isHalf}
+                  imageLeft={imageLeft}
+                  imageText={imageText}
+                >
+                  <h2 className="text-md mb-4 mt-4">{title}</h2>
+                  <PortableText blocks={text} />
+                </TextImage>
+              </div>
+            );
+          } else if (content._type === 'contactSection') {
+            const rawContent = _rawAdditionalContent.find(
+              x => x._key === content._key
+            );
+
+            if (!rawContent) return null;
+
+            const {
+              title,
+              persons
+            } = rawContent;
+            return (
+              <>
+                {form && (
+                  <div className="mt-16 py-4">
+                    <ContactSection
+                      form={form}
+                      heading={title}
+                      persons={persons}
+                    />
+                  </div>
+                )}
+              </>
+            )
+          }
+          return null;
+        })}
+
       </Layout>
     </>
   );
